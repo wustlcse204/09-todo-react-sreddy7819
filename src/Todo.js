1bf3dea763ab38da56a58dd46d9105aa48aeb0b8
sreddy7819 <59773767+sreddy7819@users.noreply.github.com>
2020-12-10 11:10:12 -0500

    Create Todo.js

---
+++ src/Todo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Todo.css';
+
+class Todo extends Component {
+  render() {
+    return (
+        <aside id="placenewones">
+            </aside>
+    );
+  }
+}
+
+export default Todo;
