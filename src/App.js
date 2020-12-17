10c1af12c9517315b74575cae2c369e84f77e4e2
sreddy7819 <59773767+sreddy7819@users.noreply.github.com>
2020-12-17 18:53:58 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,21 +2,118 @@ import React, { Component } from 'react';
 import './App.css';
 import Todo from './Todo';
 import NewTodo from './NewTodo';
-
-
+ 
+var apikey = "c0558a-9ac24e-e69337-481320-5741bc"
+ 
 class App extends Component {
- render(){
-  return(
-  <section id = "todos">
-  <NewTodo/>
-  <Todo/>
-  </section>
-
-
-
-
-
-  );
+ constructor(props) {
+   super(props)
+   this.state = {
+     todos: [],
+     input: ''
+   }
+   this.todoadd = this.todoadd.bind(this);
+   this.emptytodo = this.emptytodo.bind(this);
+   this.onChange = this.onChange.bind(this);
  }
+ 
+ onChange(event){
+   console.log(event.target.value);
+   this.setState({
+     input: event.target.value
+   });
+ }
+ 
+ todoadd(event) {
+     event.preventDefault();
+       var s = this;
+       var data = {
+         text: s.state.input
+       };
+ 
+       var makereq = new XMLHttpRequest();
+       makereq.onreadystatechange = function(){
+           if (this.readyState === 4 && this.status === 200) {
+             var t = [...s.state.todos, JSON.parse(this.responseText)]
+             t.sort(function (a, b){
+ 
+               return a.text.localeCompare(b.text);
+             })
+             s.setState({
+               todos: t
+               })
+             s.setState({
+               input: ''
+             });
+           } else if (this.readyState === 4) {
+             console.log(this.responseText);
+           }
+       };
+       makereq.open("POST", "https://cse204.work/todos", true);
+       makereq.setRequestHeader("Content-type", "application/json");
+       makereq.setRequestHeader("x-api-key", apikey);
+       makereq.send(JSON.stringify(data));
+ 
+ }
+ 
+ 
+  emptytodo(id){
+    console.log(id);
+       var s = this;
+ 
+ 
+       var removereq = new XMLHttpRequest();
+       removereq.onreadystatechange = function(){
+         if (this.readyState === 4 && this.status === 200) {
+           const rtodo = s.state.todos.filter((todo) => {
+             if (todo.id !== id){
+               return todo;
+             }
+           });
+           s.setState({ todos: rtodo });
+         } else if (this.readyState === 4) {
+           console.log(this.responseText);
+         }
+       }
+       removereq.open("DELETE", "https://cse204.work/todos/" + id, true);
+       removereq.setRequestHeader("Content-type", "application/json");
+       removereq.setRequestHeader("x-api-key", apikey);
+       removereq.send();
+ 
+     }
+ 
+ 
+ 
+ render() {
+   return (
+     <section id= "itemstodo"> <NewTodo todoadd={this.todoadd} input={this.state.input} onChange={this.onChange}/>
+     {this.state.todos.map((todo) =>
+       <Todo key={todo.id} id={todo.id} completed={todo.completed}
+       text={todo.text} emptytodo={this.emptytodo}/>
+     )}
+     </section>
+ 
+   );
+ }
+ 
+ componentDidMount(){
+   var s = this;
+   var denotereq = new XMLHttpRequest();
+   denotereq.onreadystatechange = function() {
+   if (this.readyState === 4 && this.status === 200) {
+       var todos = JSON.parse(this.responseText);
+       s.setState({todos: todos});
+     } else if (this.readyState === 4){
+       console.log(this.responseText);
+     }
+   }
+   denotereq.open("GET", "https://cse204.work/todos", true);
+   denotereq.setRequestHeader("x-api-key",apikey);
+   denotereq.send();
+ }
+ 
+ 
 }
+ 
+ 
 export default App;
