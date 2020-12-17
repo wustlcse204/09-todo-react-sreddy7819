ff6d7ed4aa033760d325312e36babb7887d92718
sreddy7819 <59773767+sreddy7819@users.noreply.github.com>
2020-12-17 18:54:07 -0500

    Update Todo.js

--- src/Todo.js
+++ src/Todo.js
@@ -1,17 +1,59 @@
 import React, { Component, Fragment } from 'react';
 import './Todo.css';
-
+var apikey = "c0558a-9ac24e-e69337-481320-5741bc"
 class Todo extends Component {
-  render(){
-   return (
-    <section id="itemstodo">
-    <aside id="placenewones">
-    <article id = "1" class="activity"><button class = "done"></button>ball<button class = "remove">remove</button> </article>
-
-    </aside>
-    </section>
+ constructor(props){
+   super(props);
+   this.state = {
+     completed: this.props.completed
+   }
+   this.ctd = this.ctd.bind(this);
+   this.emptytodo = this.emptytodo.bind(this);
+ }
+ 
+ ctd(event){
+   console.log(this.props.id);
+   var data = {
+     completed: true
+   }
+ 
+   var s = this;
+ 
+   var creq = new XMLHttpRequest();
+   creq.onreadystatechange = function () {
+     if (this.readyState === 4 && this.status == 200) {
+ 
+       s.setState({
+         completed: true
+       })
+     } else if (this.readyState === 4) {
+       console.log(this.responseText);
+     }
+   }
+   creq.open("PUT", "https://cse204.work/todos/" + s.props.id, true);
+   creq.setRequestHeader("Content-type", "application/json");
+   creq.setRequestHeader("x-api-key", apikey);
+   creq.send(JSON.stringify(data));
+ 
+ }
+ 
+ emptytodo(event){
+   this.props.emptytodo(this.props.id);
+ }
+ 
+ render() {
+     let tdclass = "activity";
+     if (this.state.completed) {
+       tdclass = "activity finish"
+     }
+     return (
+       <article id={this.props.id} className={tdclass}>
+         <button className="done" onClick={this.ctd}></button>
+         <p>{this.props.text}</p>
+         <button className="remove"  onClick={this.emptytodo}> Remove </button>
+         
+       </article>
    );
-  }
+ }
 }
 export default Todo;
-  
